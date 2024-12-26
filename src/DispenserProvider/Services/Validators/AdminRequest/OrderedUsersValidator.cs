22df2908c11ff3a7a27466f3f7233869281c6148
Stan Goldin <salty87@gmail.com>
2024-12-26 17:28:56 +0200

    refactor

--- src/DispenserProvider/Services/Validators/AdminRequest/OrderedUsersValidator.cs
+++ src/DispenserProvider/Services/Validators/AdminRequest/OrderedUsersValidator.cs
@@ -11,7 +11,8 @@ public class OrderedUsersValidator : AbstractValidator<IEnumerable<EthereumAddre
             .Cascade(CascadeMode.Stop)
             .NotEmpty()
             .WithMessage("Collection of users cannot be empty.")
-            .Must(users => !users.Zip(users.Skip(1), (left, right) => (left, right)).Any(pair => string.Compare(pair.left, pair.right) >= 0))
+            .Must(users => users.Zip(users.Skip(1), (left, right) => (left, right))
+                .All(pair => string.Compare(pair.left, pair.right) < 0))
             .WithMessage("All addresses must be unique and in ascending order.");
     }
 }
