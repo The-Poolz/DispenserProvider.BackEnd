26e662dc004f7b5f9b5ae34a289e29168fb183af
Stan Goldin <salty87@gmail.com>
2024-12-26 20:36:03 +0200

    remove =

--- src/DispenserProvider/Services/Validators/AdminRequest/OrderedUsersValidator.cs
+++ src/DispenserProvider/Services/Validators/AdminRequest/OrderedUsersValidator.cs
@@ -20,7 +20,7 @@ public class OrderedUsersValidator : AbstractValidator<IEnumerable<EthereumAddre
     private static string MessageFormat(IEnumerable<EthereumAddress> _, (EthereumAddress First, EthereumAddress Second) pair) =>
         pair.First == pair.Second
             ? $"Duplicate address found: {pair.First}"
-            : $"Addresses must be in ascending order. Found '{pair.First}' >= '{pair.Second}'";
+            : $"Addresses must be in ascending order. Found '{pair.First}' > '{pair.Second}'";
     private static IEnumerable<(EthereumAddress, EthereumAddress)> GetZippedPairs(IEnumerable<EthereumAddress> users) =>
         users.Zip(users.Skip(1));
     private static bool IsSorted((EthereumAddress First, EthereumAddress Second) pair) =>
