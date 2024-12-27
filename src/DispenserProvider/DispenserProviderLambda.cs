a3c0917a8b5eeb136965ed6c06ac57c5ef8cfec6
Stanislav Vysotskyi <ardenhide@gmail.com>
2024-12-27 18:58:13 +0300

    Set real signature

--- src/DispenserProvider/DispenserProviderLambda.cs
+++ src/DispenserProvider/DispenserProviderLambda.cs
@@ -1,6 +1,9 @@
+using Newtonsoft.Json;
+using Nethereum.Signer;
 using Amazon.Lambda.Core;
 using DispenserProvider.Models;
 using DispenserProvider.Services;
+using EnvironmentManager.Static;
 using Microsoft.Extensions.DependencyInjection;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -13,6 +16,16 @@ public class DispenserProviderLambda(IServiceProvider serviceProvider)
 
     public IHandlerResponse Run(LambdaRequest request)
     {
+        request = SetRealSignature(request);
         return serviceProvider.GetRequiredService<IHandlerFactory>().Handle(request);
+    }
+
+    private static LambdaRequest SetRealSignature(LambdaRequest request)
+    {
+        var message = JsonConvert.SerializeObject(request.CreateAssetRequest!.Message, Formatting.None);
+        var pk = EnvManager.GetRequired<string>("PRIVATE_KEY");
+        var signature = new EthereumMessageSigner().EncodeUTF8AndSign(message, new EthECKey(pk));
+        request.CreateAssetRequest.Signature = signature;
+        return request;
     }
 }
