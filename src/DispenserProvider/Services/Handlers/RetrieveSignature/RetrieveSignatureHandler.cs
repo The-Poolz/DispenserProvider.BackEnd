e3ec417d3998bc37e2b4fe66d47d1f1be4047d5d
Stanislav Vysotskyi <ardenhide@gmail.com>
2024-12-30 14:22:52 +0300

    Implement `RetrieveSignature` handler

--- src/DispenserProvider/Services/Handlers/RetrieveSignature/RetrieveSignatureHandler.cs
+++ src/DispenserProvider/Services/Handlers/RetrieveSignature/RetrieveSignatureHandler.cs
@@ -1,11 +1,14 @@
-﻿using DispenserProvider.Services.Handlers.RetrieveSignature.Models;
+﻿using DispenserProvider.DataBase;
+using DispenserProvider.Services.Handlers.RetrieveSignature.Models;
 
 namespace DispenserProvider.Services.Handlers.RetrieveSignature;
 
-public class RetrieveSignatureHandler : IRequestHandler<RetrieveSignatureRequest, RetrieveSignatureResponse>
+public class RetrieveSignatureHandler(DispenserContext dispenserContext) : IRequestHandler<RetrieveSignatureRequest, RetrieveSignatureResponse>
 {
     public RetrieveSignatureResponse Handle(RetrieveSignatureRequest request)
     {
+
+
         throw new NotImplementedException($"The 'Handle' method is not implemented, in '{nameof(RetrieveSignatureHandler)}' class.");
     }
 }
