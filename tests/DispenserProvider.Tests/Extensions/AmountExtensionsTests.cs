ca5fd5aabfbe13e53762adcb0ece01a9cd3d2687
Stan Goldin <salty87@gmail.com>
2024-12-30 15:52:41 +0200

    test const

--- tests/DispenserProvider.Tests/Extensions/AmountExtensionsTests.cs
+++ tests/DispenserProvider.Tests/Extensions/AmountExtensionsTests.cs
@@ -8,13 +8,14 @@ public class AmountExtensionsTests
 {
     public class CalculateAmount
     {
+        public const string zero18 = "000000000000000000"; // 18 zeros
         [Theory]
         [InlineData("100", 1.0, "100")]
         [InlineData("100", 1.5, "150")]
         [InlineData("100", 0.8, "80")]
         [InlineData("100", 0.0, "0")]
-        [InlineData("1000000000000000000", 0.667, "667000000000000000")]
-        [InlineData("100000000000000000000000000000", 0.667, "66700000000000000000000000000")]
+        [InlineData("1" + zero18, 0.667, "667000000000000000")]
+        [InlineData("100000000000" + zero18, 0.667, "66700000000" + zero18)]
         public void WhenCalledOnString_ShouldReturnCorrectBigIntegerString(string weiAmount, decimal ratio, string expected)
         {
             var result = weiAmount.MultiplyWeiByRatio(ratio);
