0404622f1eb815cef6e1d5b68f9e22ed782c5b6c
Stanislav Vysotskyi <ardenhide@gmail.com>
2025-01-02 17:51:57 +0300

    - fix null reference object error

--- src/DispenserProvider/Services/Handlers/RetrieveSignature/Models/RetrieveSignatureResponse.cs
+++ src/DispenserProvider/Services/Handlers/RetrieveSignature/Models/RetrieveSignatureResponse.cs
@@ -3,9 +3,10 @@ using DispenserProvider.DataBase.Models;
 
 namespace DispenserProvider.Services.Handlers.RetrieveSignature.Models;
 
-public class RetrieveSignatureResponse(DispenserDTO dispenser, RetrieveSignatureRequest request) : IHandlerResponse
+public class RetrieveSignatureResponse(Asset asset) : IHandlerResponse
 {
-    public RetrieveSignatureResponse() : this(default!, default!) { }
+    public RetrieveSignatureResponse() : this(default!) { }
+    public RetrieveSignatureResponse(DispenserDTO dispenser, RetrieveSignatureRequest request) : this(new Asset(dispenser, request)) { }
 
-    public Asset Asset { get; } = new(dispenser, request);
+    public Asset Asset { get; } = asset;
 }
