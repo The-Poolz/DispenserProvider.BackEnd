ee1cb11cb64b14338f22bb84e77320163c42e204
Stanislav Vysotskyi <66203238+ArdenHide@users.noreply.github.com>
2025-01-02 14:59:23 +0300

    CreateAssetRequest: Move Ratio logic into new method (#16)
    * CreateAssetRequest: Move Ratio logic into new method
    
    * - cleanup
    
    * - use BigDecimal
    
    * - simplify test
    
    * - cleanup
    
    * - use `BigInteger`
    - update tests
    
    * - remove two `CalculateAmount` funcs
    
    * - add one more test
    
    * fix precision (#20)
    
    * fix precision
    
    * clear
    
    * cleaer
    
    * new line
    
    * row
    
    * test const
    
    * format
    
    * add big test
    
    * GPT keep going
    
    * add more test
    
    * - simplify
    
    * - beautify
    
    ---------
    
    Co-authored-by: Stanislav Vysotskyi <ardenhide@gmail.com>
    
    ---------
    
    Co-authored-by: Stan Goldin <48094744+Lomet@users.noreply.github.com>

---
+++ src/DispenserProvider/Extensions/AmountExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Numerics;
+using Nethereum.Util;
+
+namespace DispenserProvider.Extensions;
+
+public static class AmountExtensions
+{
+    public static string MultiplyWeiByRatio(this string weiAmount, decimal ratio, int precision = 18) =>
+        (BigInteger.Parse(weiAmount) * ConvertToWei(ratio, precision) / BigInteger.Pow(10, precision)).ToString();
+    private static BigInteger ConvertToWei(decimal ratio, int precision) => new UnitConversion().ToWei(ratio, precision);
+}
