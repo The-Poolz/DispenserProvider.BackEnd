9d3956fabc539891d208d247cf930b11023ec2c0
Stanislav Vysotskyi <ardenhide@gmail.com>
2025-01-06 12:44:05 +0300

    ListOfAssets: Track already finished assets

---
+++ src/DispenserProvider/Services/Handlers/ListOfAssets/Models/DatabaseWrappers/TakenTrack.cs
@@ -0,0 +1,14 @@
+﻿using DispenserProvider.DataBase.Models;
+using DispenserProvider.Services.Validators.GenerateSignature;
+
+namespace DispenserProvider.Services.Handlers.ListOfAssets.Models.DatabaseWrappers;
+
+public sealed class TakenTrack : TakenTrackDTO
+{
+    public TakenTrack(string errorCode, DispenserDTO dispenser)
+    {
+        IsRefunded = errorCode == AssetAvailabilityValidator.Refunded;
+        Dispenser = dispenser;
+        DispenserId = dispenser.Id;
+    }
+};
--- src/DispenserProvider/Services/Validators/GenerateSignature/AssetAvailabilityValidator.cs
+++ src/DispenserProvider/Services/Validators/GenerateSignature/AssetAvailabilityValidator.cs
@@ -6,16 +6,23 @@ namespace DispenserProvider.Services.Validators.GenerateSignature;
 
 public class AssetAvailabilityValidator : AbstractValidator<DispenserDTO>
 {
+    public const string Withdrawn = "withdrawn";
+    public const string Refunded = "refunded";
+
     public AssetAvailabilityValidator(IDispenserProviderContract dispenserContract)
     {
+        ClassLevelCascadeMode = CascadeMode.Stop;
+
         RuleFor(x => x)
             .Must(x => !dispenserContract.IsTaken(x.WithdrawalDetail.ChainId, x.WithdrawalDetail.PoolId, x.UserAddress))
-            .WithMessage(ErrorMessage("withdrawn"));
+            .WithMessage(ErrorMessage(Withdrawn))
+            .WithErrorCode(Withdrawn);
 
         RuleFor(x => x)
             .Must(x => !dispenserContract.IsTaken(x.RefundDetail!.ChainId, x.RefundDetail.PoolId, x.UserAddress))
             .When(x => x.RefundDetail != null)
-            .WithMessage(ErrorMessage("refunded"));
+            .WithMessage(ErrorMessage(Refunded))
+            .WithErrorCode(Refunded);
     }
 
     private static string ErrorMessage(string status) => $"Cannot generate signature, because asset already {status}.";
