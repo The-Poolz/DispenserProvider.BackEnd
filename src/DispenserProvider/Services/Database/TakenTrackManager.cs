38b25dbbe88776c11fe6259f52696af66cdbfc6f
Stanislav Vysotskyi <ardenhide@gmail.com>
2025-01-31 12:39:41 +0300

    - refactor `ProcessTakenTracks`

--- src/DispenserProvider/Services/Database/TakenTrackManager.cs
+++ src/DispenserProvider/Services/Database/TakenTrackManager.cs
@@ -11,18 +11,18 @@ public class TakenTrackManager(IDbContextFactory<DispenserContext> dispenserCont
     public IEnumerable<DispenserDTO> ProcessTakenTracks(IEnumerable<DispenserDTO> dispensers)
     {
         var dispenserContext = dispenserContextFactory.CreateDbContext();
+
         var processed = new List<DispenserDTO>();
+        dispensers.Where(dispenser => dispenser.TakenTrack == null)
+            .ToList()
+            .ForEach(dispenser =>
+            {
+                var validation = assetValidator.Validate(dispenser);
+                if (validation.IsValid) return;
 
-        dispensers.ToList().ForEach(dispenser =>
-        {
-            if (dispenser.TakenTrack != null) return;
-
-            var validation = assetValidator.Validate(dispenser);
-            if (validation.IsValid) return;
-
-            dispenserContext.TakenTrack.Add(new TakenTrack(validation.Errors[0].ErrorCode, dispenser));
-            processed.Add(dispenser);
-        });
+                dispenserContext.TakenTrack.Add(new TakenTrack(validation.Errors[0].ErrorCode, dispenser));
+                processed.Add(dispenser);
+            });
 
         if (processed.Count > 0) dispenserContext.SaveChanges();
 
