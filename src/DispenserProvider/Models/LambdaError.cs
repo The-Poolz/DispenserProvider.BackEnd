42dc1ce0e1f5e7f1e350deeaf185aa245051cf81
Stanislav Vysotskyi <ardenhide@gmail.com>
2025-02-03 16:31:15 +0300

    - create `lambdaResponse` with error handling

---
+++ src/DispenserProvider/Models/LambdaError.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using Newtonsoft.Json.Linq;
+
+namespace DispenserProvider.Models;
+
+public class LambdaError
+{
+    public LambdaError(Exception exception)
+    {
+        ErrorType = exception.GetType().Name;
+        ErrorMessage = exception.Message;
+    }
+
+    public LambdaError(ValidationException exception)
+    {
+        var error = exception.Errors.First();
+        ErrorType = exception.GetType().Name;
+        ErrorMessage = error.ErrorMessage;
+        Data = error.CustomState == null ? null : JObject.FromObject(error.CustomState);
+    }
+
+    public string ErrorMessage { get; }
+    public string ErrorType { get; }
+    public JObject? Data { get; }
+}
---
+++ src/DispenserProvider/Models/LambdaResponse.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+
+namespace DispenserProvider.Models;
+
+public class LambdaResponse
+{
+    public LambdaResponse(IHandlerResponse data)
+    {
+        Data = data;
+    }
+
+    public LambdaResponse(Exception exception)
+    {
+        Error = new LambdaError(exception);
+    }
+
+    public LambdaResponse(ValidationException exception)
+    {
+        Error = new LambdaError(exception);
+    }
+
+    public IHandlerResponse? Data { get; }
+    public LambdaError? Error { get; }
+}
