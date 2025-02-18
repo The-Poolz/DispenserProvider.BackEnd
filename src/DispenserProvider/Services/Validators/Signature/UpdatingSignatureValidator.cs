0846cec063557780718716851b884bc5a5a6eaad
ArdenHide <ardenhide@gmail.com>
2025-02-18 16:02:41 +0300

    withdraw/refund for first time

--- src/DispenserProvider/Services/Validators/Signature/UpdatingSignatureValidator.cs
+++ src/DispenserProvider/Services/Validators/Signature/UpdatingSignatureValidator.cs
@@ -3,28 +3,30 @@ using FluentValidation;
 using EnvironmentManager.Extensions;
 using DispenserProvider.DataBase.Models;
 using Net.Utils.ErrorHandler.Extensions;
+using DispenserProvider.Services.Validators.Signature.Models;
 
 namespace DispenserProvider.Services.Validators.Signature;
 
-public class UpdatingSignatureValidator : AbstractValidator<DispenserDTO>
+public class UpdatingSignatureValidator : AbstractValidator<GenerateSignatureValidatorRequest>
 {
     public UpdatingSignatureValidator()
     {
-        RuleFor(x => x.LastUserSignature!)
+        RuleFor(x => x.Dispenser.LastUserSignature!)
             .Cascade(CascadeMode.Stop)
             .Must(x => DateTime.UtcNow >= x.ValidUntil)
             .WithError(ErrorCode.SIGNATURE_IS_STILL_VALID, x => new
             {
-                ValidFrom = x.LastUserSignature!.ValidFrom.ToUnixTimestamp(),
-                NextTry = NextTry(x).ToUnixTimestamp()
+                ValidFrom = x.Dispenser.LastUserSignature!.ValidFrom.ToUnixTimestamp(),
+                NextTry = NextTry(x.Dispenser).ToUnixTimestamp()
             })
             .Must(x => DateTime.UtcNow >= NextTry(x))
+            .When(x => x.IsRefund != x.Dispenser.LastUserSignature!.IsRefund)
             .WithError(ErrorCode.SIGNATURE_GENERATION_VALID_TIME_NOT_ARRIVED, x => new
             {
-                NextTry = NextTry(x).ToUnixTimestamp()
+                NextTry = NextTry(x.Dispenser).ToUnixTimestamp()
             });
     }
 
     public static DateTime NextTry(SignatureDTO signature) => signature.ValidUntil + TimeSpan.FromSeconds(Env.COOLDOWN_OFFSET_IN_SECONDS.GetRequired<int>());
-    private static DateTime NextTry(DispenserDTO dispenser) => NextTry(dispenser.LastUserSignature!);
+    public static DateTime NextTry(DispenserDTO dispenser) => NextTry(dispenser.LastUserSignature!);
 }
