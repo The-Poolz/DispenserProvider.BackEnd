954647177a03f3a0b98b1d382ecc8d086b015d82
Stanislav Vysotskyi <66203238+ArdenHide@users.noreply.github.com>
2025-02-21 16:18:09 +0300

    Receive DispenserProvider contract address from Db (#76)
    * Receive DispenserProvider contract address from Db
    
    * - update tests

--- tests/DispenserProvider.Tests/Mocks/DataBase/MockCovalentContext.cs
+++ tests/DispenserProvider.Tests/Mocks/DataBase/MockCovalentContext.cs
@@ -1,4 +1,5 @@
 ﻿using CovalentDb;
+using CovalentDb.Types;
 using CovalentDb.Models;
 using TestableDbContext.Mock;
 using ConfiguredSqlConnection.Extensions;
@@ -14,13 +15,25 @@ internal static class MockCovalentContext
         RpcConnection = "http://localhost"
     };
 
+    internal static DownloaderSettings DispenserTokensDispensed => new()
+    {
+        EventHash = "hash",
+        Key = "key",
+        UrlSet = "url",
+        ChainId = 97,
+        ResponseType = ResponseType.DispenserTokensDispensed,
+        ContractAddress = "0x0000000000000000000000000000000000000001"
+    };
+
     internal static CovalentContext Create()
     {
         var context = new DbContextFactory<CovalentContext>().Create(ContextOption.InMemory, $"{Guid.NewGuid()}");
 
         var chains = new List<Chain> { Chain }.AsQueryable().BuildMockDbSet();
+        var downloaderSettings = new List<DownloaderSettings> { DispenserTokensDispensed }.AsQueryable().BuildMockDbSet();
 
         context.Chains.AddRange(chains.Object);
+        context.DownloaderSettings.AddRange(downloaderSettings.Object);
         context.SaveChanges();
 
         return context;
