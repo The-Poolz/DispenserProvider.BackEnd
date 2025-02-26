233d6ef26bb2535c1b99891fad209f510a5b69ea
ArdenHide <ardenhide@gmail.com>
2025-02-26 14:47:43 +0300

    - remove `RatioExtensions` - simplify `AmountExtensions` - update `AmountExtensionsTests`

--- src/DispenserProvider/Extensions/RatioExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿using Nethereum.Util;
-using System.Numerics;
-
-namespace DispenserProvider.Extensions;
-
-public static class RatioExtensions
-{
-    public static decimal StringRatioToDecimal(this string ratio) => UnitConversion.Convert.FromWei(BigInteger.Parse(ratio), 18);
-}
