41df7c765c724197300c6093e5c72356315ed191
ArdenHide <ardenhide@gmail.com>
2025-03-04 16:09:40 +0300

    - remove `Web3Contract` nesting

--- src/DispenserProvider/Services/Web3/Contracts/BuilderContract.cs
+++ src/DispenserProvider/Services/Web3/Contracts/BuilderContract.cs
@@ -3,11 +3,11 @@ using poolz.finance.csharp.contracts.LockDealNFT;
 
 namespace DispenserProvider.Services.Web3.Contracts;
 
-public class BuilderContract(IChainProvider chainProvider) : Web3Contract(chainProvider), IBuilderContract
+public class BuilderContract(IChainProvider chainProvider) : IBuilderContract
 {
     public string Name(long chainId, EthereumAddress address)
     {
-        var web3 = ChainProvider.Web3(chainId);
+        var web3 = chainProvider.Web3(chainId);
         var contract = new LockDealNFTService(web3, address);
         return contract.NameQueryAsync().GetAwaiter().GetResult();
     }
