23ba895a0b97d5f6413a0891660960eb5e415e24
ArdenHide <ardenhide@gmail.com>
2025-08-26 13:02:51 +0300

    - update saved requests of `ListOfAssetsRequest` - update packages versions - update env - update `DefaultServiceProvider`

--- src/DispenserProvider/Services/DefaultServiceProvider.cs
+++ src/DispenserProvider/Services/DefaultServiceProvider.cs
@@ -1,4 +1,6 @@
-﻿using FluentValidation;
+﻿using Amazon;
+using Amazon.RDS.Util;
+using FluentValidation;
 using System.Reflection;
 using DispenserProvider.Options;
 using DispenserProvider.DataBase;
@@ -57,6 +59,20 @@ public static class DefaultServiceProvider
         .AddScoped<ISignerManager, SignerManager>();
 
     private static IServiceCollection Stage => new ServiceCollection()
-        .AddDbContextFactory<DispenserContext>(options => options.UseSqlServer(ConnectionStringFactory.GetConnectionFromConfiguration("DispenserStage")))
-        .AddScoped<ISignerManager, SignerManager>();
+        .AddDbContextFactory<DispenserContext>(options =>
+        {
+            var hostname = Env.STAGE_POSTGRES_HOSTNAME.GetRequired();
+            var port = Env.STAGE_POSTGRES_PORT.GetRequired<int>();
+            var dbUser = Env.STAGE_POSTGRES_DB_USER.GetRequired();
+            var dbName = Env.STAGE_POSTGRES_DB_NAME.GetRequired();
+            var sslCertPath = Env.STAGE_POSTGRES_SSL_CERT_FULL_PATH.GetRequired();
+            var region = RegionEndpoint.AFSouth1; // Need to make wrapper to receive it from env, or keep hard-code.
+
+            var pwd = RDSAuthTokenGenerator.GenerateAuthToken(region, hostname, port, dbUser);
+
+            var connectionString = $"Server={hostname};User Id={dbUser};Password={pwd};Database={dbName};SSL Mode=Require;Root Certificate={sslCertPath}";
+
+            options.UseNpgsql(connectionString);
+        })
+        .AddScoped<ISignerManager, EnvSignerManager>();
 }
