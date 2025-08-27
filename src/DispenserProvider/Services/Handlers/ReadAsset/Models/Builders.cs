0e9a024ce390ce12b0c20b26e87d096c8899ef7a
ArdenHide <ardenhide@gmail.com>
2025-08-27 13:04:36 +0300

    - fix `ReadAsset`

--- src/DispenserProvider/Services/Handlers/ReadAsset/Models/Builders.cs
+++ src/DispenserProvider/Services/Handlers/ReadAsset/Models/Builders.cs
@@ -11,8 +11,8 @@ public class Builder(BuilderDTO builder)
     public string WeiAmount { get; } = builder.WeiAmount;
 
     [JsonConverter(typeof(UnixDateTimeConverter))]
-    public DateTime? StartTime { get; } = builder.StartTime;
+    public DateTime? StartTime { get; } = builder.StartTime.HasValue ? DateTime.SpecifyKind(builder.StartTime.Value, DateTimeKind.Utc) : null;
 
     [JsonConverter(typeof(UnixDateTimeConverter))]
-    public DateTime? FinishTime { get; } = builder.FinishTime;
+    public DateTime? FinishTime { get; } = builder.FinishTime.HasValue ? DateTime.SpecifyKind(builder.FinishTime.Value, DateTimeKind.Utc) : null;
 }
