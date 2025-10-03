b6175d5381bd538d143dbbd0dd02d115cda8e448
ArdenHide <ardenhide@gmail.com>
2025-10-03 16:28:03 +0300

    - fix test

--- tests/DispenserProvider.Tests/Services/Handlers/GenerateSignature/Web3/ChainProviderTests.cs
+++ tests/DispenserProvider.Tests/Services/Handlers/GenerateSignature/Web3/ChainProviderTests.cs
@@ -12,13 +12,15 @@ public class ChainProviderTests
         [Fact]
         internal void WhenChainIdIsSupported_ShouldReturnsWeb3()
         {
-            Environment.SetEnvironmentVariable("BASE_URL_OF_RPC", "http://localhost:5050");
+            var chainId = 97;
+            var baseUrl = "https://rpc/evm/";
+            Environment.SetEnvironmentVariable(nameof(Env.BASE_URL_OF_RPC), baseUrl);
 
             var strapi = new MockStrapiClient(MockStrapiClient.DefaultOnChainInfo);
 
             var chainProvider = new ChainProvider(strapi);
 
-            var response = chainProvider.Web3(97);
+            var response = chainProvider.Web3(chainId);
 
             response.Should().NotBeNull();
         }
