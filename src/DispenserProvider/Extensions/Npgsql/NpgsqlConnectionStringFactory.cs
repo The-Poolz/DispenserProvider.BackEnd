1f547f7d38e1a175315bc86c77de69aa249211f4
ArdenHide <ardenhide@gmail.com>
2025-10-03 17:04:46 +0300

    - refactor `NpgsqlConnectionStringFactory`

--- src/DispenserProvider/Extensions/Npgsql/NpgsqlConnectionStringFactory.cs
+++ src/DispenserProvider/Extensions/Npgsql/NpgsqlConnectionStringFactory.cs
@@ -1,7 +1,6 @@
 ﻿using Npgsql;
 using Amazon;
 using SecretsManager;
-using Amazon.RDS.Util;
 using EnvironmentManager.Core;
 using DispenserProvider.Converters;
 using EnvironmentManager.Extensions;
@@ -10,9 +9,9 @@ namespace DispenserProvider.Extensions.Npgsql;
 
 public static class NpgsqlConnectionStringFactory
 {
-    public static string BuildProd()
+    public static string BuildProd(SecretManager? secretManager = null)
     {
-        var secretManager = new SecretManager();
+        secretManager ??= new SecretManager();
         var secretId = Env.PROD_POSTGRES_SECRET_ID.GetRequired();
         var builder = new NpgsqlConnectionStringBuilder
         {
@@ -27,7 +26,7 @@ public static class NpgsqlConnectionStringFactory
         return builder.ConnectionString;
     }
 
-    public static string BuildStage()
+    public static string BuildStage(IRdsAuthTokenGenerator? rdsAuthTokenGenerator = null)
     {
         var hostname = Env.STAGE_POSTGRES_HOSTNAME.GetRequired();
         var port = Env.STAGE_POSTGRES_PORT.GetRequired<int>();
@@ -35,7 +34,8 @@ public static class NpgsqlConnectionStringFactory
 
         var envManager = new EnvManager(MapperConfigurationsExtensions.WithAwsRegionConverters());
         var region = envManager.Get<RegionEndpoint?>(nameof(Env.STAGE_POSTGRES_AWS_REGION)) ?? RegionEndpoint.EUCentral1;
-        var password = RDSAuthTokenGenerator.GenerateAuthToken(region, hostname, port, dbUser);
+        rdsAuthTokenGenerator ??= new RdsAuthTokenGenerator();
+        var password = rdsAuthTokenGenerator.GenerateAuthToken(region, hostname, port, dbUser);
 
         var builder = new NpgsqlConnectionStringBuilder
         {
---
+++ src/DispenserProvider/Extensions/Npgsql/RdsAuthTokenGenerator.cs
@@ -0,0 +1,12 @@
+﻿using Amazon;
+using Amazon.RDS.Util;
+
+namespace DispenserProvider.Extensions.Npgsql;
+
+public class RdsAuthTokenGenerator : IRdsAuthTokenGenerator
+{
+    public string GenerateAuthToken(RegionEndpoint region, string hostname, int port, string dbUser)
+    {
+        return RDSAuthTokenGenerator.GenerateAuthToken(region, hostname, port, dbUser);
+    }
+}
