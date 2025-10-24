d6d34124adb0a50286a34e76aff2ea6e6dece581
ArdenHide <ardenhide@gmail.com>
2025-10-24 16:54:44 +0300

    - remove bsc testnet avoiding

--- src/DispenserProvider/Extensions/TestnetChainsManager.cs
+++ src/DispenserProvider/Extensions/TestnetChainsManager.cs
@@ -2,5 +2,8 @@
 
 public static class TestnetChainsManager
 {
-    public static long[] TestnetChains = [97, 11155111, 7082400];
+    public const long BSC_TESTNET = 97;
+    public const long ETHEREUM_SEPOLIA = 11155111;
+    public const long COTI_TESTNET = 7082400;
+    public static long[] TestnetChains = [ETHEREUM_SEPOLIA, COTI_TESTNET];
 }
